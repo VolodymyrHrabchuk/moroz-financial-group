62bf51bdd022b6f65e2c145fb593db3f077cf656
“VolodymyrHrabchuk” <vladimirgrbchk2804@gmail.com>
2024-09-13 16:28:12 +0300

    checking menu;

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AnimatePresence, motion } from "framer-motion";
@@ -29,21 +29,22 @@ const chevronVariants = {
   open: { rotate: 270 },
   closed: { rotate: 90 },
 };
+
 const menuVariants = {
   open: {
-    x: 0, // Slide in from the left
+    x: 0,
     opacity: 1,
     transition: {
-      staggerChildren: 0.1, // Stagger the animation of children
-      delayChildren: 0.2, // Delay the start of the children animations
+      staggerChildren: 0.1,
+      delayChildren: 0.2,
       type: "tween",
       ease: "easeOut",
       duration: 0.3,
     },
-    display: "flex", // Ensure that the menu is displayed when it is open
+    display: "flex",
   },
   closed: {
-    x: "-100%", // Slide out to the left
+    x: "-100%",
     opacity: 0,
     transition: {
       type: "tween",
@@ -51,7 +52,7 @@ const menuVariants = {
       duration: 0.3,
     },
     transitionEnd: {
-      display: "none", // Hide the menu after it has closed
+      display: "none",
     },
   },
 };
@@ -82,14 +83,14 @@ const pathVariants = {
 // Chevron Icon Component
 const ChevronIcon = ({ isOpen }) => (
   <motion.svg
-    xmlns='http://www.w3.org/2000/svg'
-    viewBox='0 0 185.343 185.343'
-    fill='white'
-    className='w-3 h-3 pl-1'
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 185.343 185.343"
+    fill="white"
+    className="w-3 h-3 pl-1"
     variants={chevronVariants}
     animate={isOpen ? "open" : "closed"}
   >
-    <path d='M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175 l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934 c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z' />
+    <path d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175 l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934 c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z" />
   </motion.svg>
 );
 
@@ -97,13 +98,13 @@ const ChevronIcon = ({ isOpen }) => (
 const Submenu = ({ links, isOpen }) => (
   <motion.ul
     variants={submenuVariants}
-    initial='closed'
+    initial="closed"
     animate={isOpen ? "open" : "closed"}
-    className='absolute left-0 md:left-[-20px] mt-4 bg-blue shadow-lg p-4 flex flex-col space-y-4 w-[120px] '
+    className="absolute left-0 md:left-[-20px] mt-4 bg-blue shadow-lg p-4 flex flex-col space-y-4 w-[120px]"
   >
     {links.map(({ href, label }) => (
       <li key={href}>
-        <Link href={href} className='text-white hover:text-gray-300'>
+        <Link href={href} className="text-white hover:text-gray-300">
           {label}
         </Link>
       </li>
@@ -122,87 +123,91 @@ const Navbar = () => {
   const toggleMobileSubmenu = () => setIsMobileSubmenuOpen((prev) => !prev);
 
   return (
-    <nav className='bg-blue text-lg fixed top-0 z-150 w-full'>
-      <div className='max-w-[1400px] mx-auto h-32 flex items-center justify-between px-10 relative'>
-        <div className='flex items-center'>
-          <Image
-            src='/logo.svg'
-            alt='Moroz Financial Group Logo'
-            width={180}
-            height={220}
-          />
-        </div>
-        <div className='md:hidden'>
-          <motion.button
-            onClick={toggleMobileMenu}
-            animate={isMobileMenuOpen ? "open" : "closed"}
-            aria-label='Toggle menu'
-          >
-            <motion.svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-8 w-8 text-white'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              variants={buttonVariants}
+    <>
+      <nav className="bg-blue text-lg fixed top-0 z-50 w-full">
+        <div className="max-w-[1400px] mx-auto h-32 flex items-center justify-between px-10 relative z-50">
+          <div className="flex items-center">
+            <Image
+              src="/logo.svg"
+              alt="Moroz Financial Group Logo"
+              width={180}
+              height={220}
+            />
+          </div>
+          <div className="md:hidden">
+            <motion.button
+              onClick={toggleMobileMenu}
+              animate={isMobileMenuOpen ? "open" : "closed"}
+              aria-label="Toggle menu"
             >
-              <motion.path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                variants={pathVariants}
-                initial={false}
-              />
-            </motion.svg>
-          </motion.button>
-        </div>
-        <ul className='hidden md:flex space-x-8 font-bold'>
-          {NAV_LINKS.map((link) => {
-            const isSubmenuOpen = activeSubmenu === link.label;
-            return (
-              <li
-                key={link.href}
-                className='relative group'
-                onMouseEnter={() =>
-                  link.hasSubmenu && handleMouseEnter(link.label)
-                }
-                onMouseLeave={() => link.hasSubmenu && handleMouseLeave()}
+              <motion.svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                variants={buttonVariants}
               >
-                <div className='flex items-center space-x-1'>
-                  <Link
-                    href={link.href}
-                    className='text-white hover:text-gray-300'
-                  >
-                    {link.label}
-                  </Link>
-                  {link.hasSubmenu && <ChevronIcon isOpen={isSubmenuOpen} />}
-                </div>
-                {link.hasSubmenu && (
-                  <Submenu links={ABOUT_SUBMENU} isOpen={isSubmenuOpen} />
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+                <motion.path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  variants={pathVariants}
+                  initial={false}
+                />
+              </motion.svg>
+            </motion.button>
+          </div>
+          <ul className="hidden md:flex space-x-8 font-bold">
+            {NAV_LINKS.map((link) => {
+              const isSubmenuOpen = activeSubmenu === link.label;
+              return (
+                <li
+                  key={link.href}
+                  className="relative group"
+                  onMouseEnter={() =>
+                    link.hasSubmenu && handleMouseEnter(link.label)
+                  }
+                  onMouseLeave={() => link.hasSubmenu && handleMouseLeave()}
+                >
+                  <div className="flex items-center space-x-1">
+                    <Link
+                      href={link.href}
+                      className="text-white hover:text-gray-300"
+                    >
+                      {link.label}
+                    </Link>
+                    {link.hasSubmenu && (
+                      <ChevronIcon isOpen={isSubmenuOpen} />
+                    )}
+                  </div>
+                  {link.hasSubmenu && (
+                    <Submenu links={ABOUT_SUBMENU} isOpen={isSubmenuOpen} />
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
 
       {/* Mobile Menu */}
       <motion.div
-        className={`${
-          isMobileMenuOpen ? "flex" : "hidden"
-        } md:hidden flex-col items-center w-full bg-blue text-white min-h-screen overflow-auto justify-center`}
-        initial='closed'
+        className={`fixed top-32 left-0 right-0 bottom-0 z-40 flex flex-col items-center justify-center bg-blue text-white md:hidden ${
+          isMobileMenuOpen ? "" : "hidden"
+        }`}
+        initial="closed"
         variants={menuVariants}
         animate={isMobileMenuOpen ? "open" : "closed"}
       >
-        <ul className='flex flex-col gap-4 w-full mb-40 items-center'>
+        <ul className="flex flex-col gap-4 items-center">
           {NAV_LINKS.map((link) => (
-            <li key={link.href} className='text-2xl'>
+            <li key={link.href} className="text-2xl">
               {link.hasSubmenu ? (
                 <>
                   <button
                     onClick={toggleMobileSubmenu}
-                    className='flex items-center justify-between w-full text-white hover:text-gray-300'
+                    className="flex items-center justify-between w-full text-white hover:text-gray-300"
                   >
                     {link.label}
                     <ChevronIcon isOpen={isMobileSubmenuOpen} />
@@ -212,16 +217,16 @@ const Navbar = () => {
                   <AnimatePresence>
                     {isMobileSubmenuOpen && (
                       <motion.ul
-                        key='submenu'
-                        initial='closed'
-                        animate='open'
-                        exit='closed'
+                        key="submenu"
+                        initial="closed"
+                        animate="open"
+                        exit="closed"
                         variants={mobileSubmenuVariants}
-                        className='text-center mt-2 space-y-2 overflow-hidden'
+                        className="text-center mt-2 space-y-2 overflow-hidden"
                       >
                         {ABOUT_SUBMENU.map((sublink) => (
                           <li key={sublink.href}>
-                            <Link href={sublink.href} className='text-white'>
+                            <Link href={sublink.href} className="text-white">
                               {sublink.label}
                             </Link>
                           </li>
@@ -233,7 +238,7 @@ const Navbar = () => {
               ) : (
                 <Link
                   href={link.href}
-                  className='text-white hover:text-gray-300'
+                  className="text-white hover:text-gray-300"
                 >
                   {link.label}
                 </Link>
@@ -242,7 +247,7 @@ const Navbar = () => {
           ))}
         </ul>
       </motion.div>
-    </nav>
+    </>
   );
 };
 
