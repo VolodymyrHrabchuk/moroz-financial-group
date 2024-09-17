a44868f94560ce0bc1565ea4397d98f787f7914e
“VolodymyrHrabchuk” <vladimirgrbchk2804@gmail.com>
2024-09-17 21:57:37 +0300

    changed closing menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 // Constants for links and submenus
 const NAV_LINKS = [
@@ -83,14 +84,14 @@ const pathVariants = {
 // Chevron Icon Component
 const ChevronIcon = ({ isOpen }) => (
   <motion.svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 185.343 185.343"
-    fill="white"
-    className="w-3 h-3 pl-1"
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 185.343 185.343'
+    fill='white'
+    className='w-3 h-3 pl-1'
     variants={chevronVariants}
     animate={isOpen ? "open" : "closed"}
   >
-    <path d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175 l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934 c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z" />
+    <path d='M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175 l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934 c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z' />
   </motion.svg>
 );
 
@@ -98,13 +99,13 @@ const ChevronIcon = ({ isOpen }) => (
 const Submenu = ({ links, isOpen }) => (
   <motion.ul
     variants={submenuVariants}
-    initial="closed"
+    initial='closed'
     animate={isOpen ? "open" : "closed"}
-    className="absolute left-0 md:left-[-20px] mt-4 bg-blue shadow-lg p-4 flex flex-col space-y-4 w-[120px]"
+    className='absolute left-0 md:left-[-20px] mt-4 bg-blue shadow-lg p-4 flex flex-col space-y-4 w-[120px]'
   >
     {links.map(({ href, label }) => (
       <li key={href}>
-        <Link href={href} className="text-white hover:text-gray-300">
+        <Link href={href} className='text-white hover:text-gray-300'>
           {label}
         </Link>
       </li>
@@ -117,40 +118,53 @@ const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isMobileSubmenuOpen, setIsMobileSubmenuOpen] = useState(false);
 
+  const pathname = usePathname();
+
   const handleMouseEnter = (label) => setActiveSubmenu(label);
   const handleMouseLeave = () => setActiveSubmenu(null);
   const toggleMobileMenu = () => setIsMobileMenuOpen((prev) => !prev);
   const toggleMobileSubmenu = () => setIsMobileSubmenuOpen((prev) => !prev);
 
+  useEffect(() => {
+    // Set a timeout to close menus after 0.5 seconds (500 milliseconds)
+    const timeoutId = setTimeout(() => {
+      setIsMobileMenuOpen(false);
+      setIsMobileSubmenuOpen(false);
+    }, 500); // 500 milliseconds = 0.5 seconds
+  
+    // Cleanup function to clear the timeout if the effect is re-run or the component unmounts
+    return () => clearTimeout(timeoutId);
+  }, [pathname]);
+
   return (
     <>
-      <nav className="bg-blue text-lg fixed top-0 z-50 w-full">
-        <div className="max-w-[1400px] mx-auto h-32 flex items-center justify-between px-6 relative z-50">
-          <div className="flex items-center">
+      <nav className='bg-blue text-lg fixed top-0 z-50 w-full'>
+        <div className='max-w-[1400px] mx-auto h-32 flex items-center justify-between px-6 relative z-50'>
+          <div className='flex items-center'>
             <Image
-              src="/logo.svg"
-              alt="Moroz Financial Group Logo"
+              src='/logo.svg'
+              alt='Moroz Financial Group Logo'
               width={180}
               height={220}
             />
           </div>
-          <div className="md:hidden">
+          <div className='md:hidden'>
             <motion.button
               onClick={toggleMobileMenu}
               animate={isMobileMenuOpen ? "open" : "closed"}
-              aria-label="Toggle menu"
+              aria-label='Toggle menu'
             >
               <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-8 w-8 text-white'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
                 variants={buttonVariants}
               >
                 <motion.path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   strokeWidth={2}
                   variants={pathVariants}
                   initial={false}
@@ -158,28 +172,26 @@ const Navbar = () => {
               </motion.svg>
             </motion.button>
           </div>
-          <ul className="hidden md:flex space-x-8 font-bold">
+          <ul className='hidden md:flex space-x-8 font-bold'>
             {NAV_LINKS.map((link) => {
               const isSubmenuOpen = activeSubmenu === link.label;
               return (
                 <li
                   key={link.href}
-                  className="relative group"
+                  className='relative group'
                   onMouseEnter={() =>
                     link.hasSubmenu && handleMouseEnter(link.label)
                   }
                   onMouseLeave={() => link.hasSubmenu && handleMouseLeave()}
                 >
-                  <div className="flex items-center space-x-1">
+                  <div className='flex items-center space-x-1'>
                     <Link
                       href={link.href}
-                      className="text-white hover:text-gray-300"
+                      className='text-white hover:text-gray-300'
                     >
                       {link.label}
                     </Link>
-                    {link.hasSubmenu && (
-                      <ChevronIcon isOpen={isSubmenuOpen} />
-                    )}
+                    {link.hasSubmenu && <ChevronIcon isOpen={isSubmenuOpen} />}
                   </div>
                   {link.hasSubmenu && (
                     <Submenu links={ABOUT_SUBMENU} isOpen={isSubmenuOpen} />
@@ -196,18 +208,18 @@ const Navbar = () => {
         className={`fixed top-0 left-0 right-0 bottom-0 z-40 flex flex-col items-center justify-center bg-blue text-white md:hidden ${
           isMobileMenuOpen ? "" : "hidden"
         }`}
-        initial="closed"
+        initial='closed'
         variants={menuVariants}
         animate={isMobileMenuOpen ? "open" : "closed"}
       >
-        <ul className="flex flex-col gap-5 items-center">
+        <ul className='flex flex-col gap-5 items-center'>
           {NAV_LINKS.map((link) => (
-            <li key={link.href} className="text-3xl">
+            <li key={link.href} className='text-3xl'>
               {link.hasSubmenu ? (
                 <>
                   <button
                     onClick={toggleMobileSubmenu}
-                    className="flex items-center justify-between w-full text-white hover:text-gray-300"
+                    className='flex items-center justify-between w-full text-white hover:text-gray-300'
                   >
                     {link.label}
                     <ChevronIcon isOpen={isMobileSubmenuOpen} />
@@ -217,16 +229,16 @@ const Navbar = () => {
                   <AnimatePresence>
                     {isMobileSubmenuOpen && (
                       <motion.ul
-                        key="submenu"
-                        initial="closed"
-                        animate="open"
-                        exit="closed"
+                        key='submenu'
+                        initial='closed'
+                        animate='open'
+                        exit='closed'
                         variants={mobileSubmenuVariants}
-                        className="text-center mt-2 space-y-2 overflow-hidden"
+                        className='text-center mt-2 space-y-2 overflow-hidden'
                       >
                         {ABOUT_SUBMENU.map((sublink) => (
                           <li key={sublink.href}>
-                            <Link href={sublink.href} className="text-white">
+                            <Link href={sublink.href} className='text-white'>
                               {sublink.label}
                             </Link>
                           </li>
@@ -238,7 +250,8 @@ const Navbar = () => {
               ) : (
                 <Link
                   href={link.href}
-                  className=" text-white hover:text-gray-300"
+                  className=' text-white hover:text-gray-300'
+                  onClick={toggleMobileMenu}
                 >
                   {link.label}
                 </Link>
