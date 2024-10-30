028af30be534c7751223d82f9beef9234ee30d4c
“VolodymyrHrabchuk” <hrbchk2804@gmail.com>
2024-10-30 13:42:14 +0200

    OG changed;

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,29 +11,31 @@ export const metadata = {
   title: "Moroz Financial Group",
   description: "Your best financial services",
   openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: 'https://moroz-financial-group.vercel.app/',
-    title: 'Moroz Financial Group',
-    description: 'Your best financial services',
+    type: "website",
+    locale: "en_US",
+    url: "https://moroz-financial-group.vercel.app/",
+    title: "Moroz Financial Group",
+    description: "Your best financial services",
     images: [
       {
-        url: 'https://moroz-financial-group.vercel.app/opengraph.webp',
+        url: "https://moroz-financial-group.vercel.app/opengraph.webp",
         width: 1200,
         height: 630,
-        alt: 'Moroz Financial Group Full Team',
+        alt: "Moroz Financial Group Full Team",
       },
     ],
-    siteName: 'Moroz Financial Group',
+    siteName: "Moroz Financial Group",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Moroz Financial Group',
-    description: 'Your best financial services',
-    images: ['https://moroz-financial-group.vercel.app/opengraph.webp'],
+    card: "summary_large_image",
+    title: "Moroz Financial Group",
+    description: "Your best financial services",
+    images: ["https://moroz-financial-group.vercel.app/opengraph.webp"],
+  },
+  other: {
+    "og:logo": "https://moroz-financial-group.vercel.app/opengraph.webp",
   },
 };
-
 
 const nanum = Nanum_Myeongjo({
   weight: ["400", "700", "800"],
